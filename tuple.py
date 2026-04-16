45244032e280b99f619a157ce0d99632403781e2
yugjani47 <yugjani265@gmail.com>
2026-04-16 11:02:13 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,10 @@
+india= ("Andhra Pradesh","Arunachal Pradesh","Assam","Bihar","Chhattisgarh","Goa","Gujarat",
+"Haryana","Himachal Pradesh","Jharkhand","Karnataka","Kerala","Madhya Pradesh","Maharashtra",
+"Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Punjab","Rajasthan","Sikkim","Tamil Nadu",
+"Telangana","Tripura","Uttar Pradesh","Uttarakhand","West Bengal")
+
+print("indian states",india)
+print(india * 5)
+print(india[2:5])
+print(india[7])
+#india.del(3)
