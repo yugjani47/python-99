0196eeaf81f8cf12da74e7f3521df9f2cf6de058
yugjani47 <yugjani265@gmail.com>
2026-04-17 10:53:07 +0530

    Delete list.py

--- list.py
+++
@@ -1,9 +0,0 @@
-favourite_destination=['kasmir','kedarnath','haridvar',
-'manali','ramesvar','kerela','andman-nikobar','lakshyadvip'
-'mizoram','dharmshala']
-print("favourite destination")
-print(favourite_destination)
-print(favourite_destination[1])
-print(favourite_destination[:6])
-print(favourite_destination * 2)
-print(favourite_destination[2:6])
